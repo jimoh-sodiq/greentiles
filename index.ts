5a078e88d8693923da23268ce7ced54e43d72669
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-09-24 07:48:33 +0100

    testing

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import * as fs from "fs";
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 
-const commitMessage =  process.argv[2] ?? "Committing Today";
+const commitMessage = process.argv[2] ?? "Committing Today";
 const commitCount = Number(process.argv[3]) ?? 5;
 
 async function runConsoleCommand(command: string) {
   try {
     console.log(`running command: ${command}`);
-    const output = exec(command, { encoding: "utf-8" });
+    const output = execSync(command, { encoding: "utf-8" });
   } catch (error: any) {
     console.error(`Error: ${error.stderr}`);
     process.exit(error.code);
@@ -22,20 +22,23 @@ function createGreenTilesFileOrAppendText() {
   fs.existsSync(".greentiles.txt")
     ? console.log("creating greentiles.txt file")
     : console.log("updating greetiles.txt");
-  fs.appendFileSync("greentiles.txt", `${commitMessage} - ${new Date().toLocaleString()}\n`);
+  fs.appendFileSync(
+    "greentiles.txt",
+    `${commitMessage} - ${new Date().toLocaleString()}\n`
+  );
 }
 
 function addAndCreateCommit() {
-  runConsoleCommand("git add .");
-  runConsoleCommand(`git commit -m "${commitMessage}"`);
+  // runConsoleCommand("git add .");
+  runConsoleCommand(`git commit -am "${commitMessage}"`);
   console.log("commit created");
 }
 
-async function pushCommit() {
-  await runConsoleCommand("git push");
+function pushCommit() {
+   runConsoleCommand("git push");
 }
 
-function run() {
+async function run() {
   checkIfGitExists();
   createGreenTilesFileOrAppendText();
   addAndCreateCommit();
